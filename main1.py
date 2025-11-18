b17bd3bfb7266a863880c246da50d062f2099094
DmytroK-hub <Kutsuperadmytro@gmail.com>
2025-11-18 18:01:04 +0200

    HW_3.1
    added calculator

---
+++ main1.py
@@ -0,0 +1,19 @@
+number1 = float(input("Веддіть число "))
+action = input("Веддіть дію ")
+number2 = float(input("Веддіть число "))
+
+if action == "+":
+	result = number1 + number2
+elif action == "-":
+	result = number1 - number2
+elif action == "*":
+	result = number1 * number2
+elif action == "/":
+	if number2 == 0:
+		print("На нуль ділити не можливо")
+	else:
+		result = number1 / number2
+else:
+	print("Такої дії не існує")
+
+print("Результат дії: \n", result)
